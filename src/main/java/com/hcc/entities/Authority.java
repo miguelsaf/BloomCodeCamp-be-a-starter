fc482768c227c9c0611b4e47b4989ebc14dd80bc
miguelsaf <miguelarcanjo03@gmail.com>
2024-09-10 13:06:19 +0100

    Update Authority entity to implement GrantedAuthority

--- src/main/java/com/hcc/entities/Authority.java
+++ src/main/java/com/hcc/entities/Authority.java
@@ -1,12 +1,22 @@
 package com.hcc.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+
 import javax.persistence.ManyToOne;
 
-public class Authority {
+public class Authority implements GrantedAuthority {
     private Long id;
     private String authority;
 
     @ManyToOne
     private User user;
 
+    public Authority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
 }
