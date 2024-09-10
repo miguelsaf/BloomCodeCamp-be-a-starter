4026ebc5bbc307ea1424a58383cf64954fe5817b
miguelsaf <miguelarcanjo03@gmail.com>
2024-09-10 12:43:08 +0100

    remove the setters and getters from username, cohortDate and password

--- src/main/java/com/hcc/entities/User.java
+++ src/main/java/com/hcc/entities/User.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,30 +25,6 @@ public class User {
     public User(LocalDate cohortStartDate, String username, String password) {
         this.cohortStartDate = cohortStartDate;
         this.username = username;
-        this.password = password;
-    }
-
-    public LocalDate getCohortStartDate() {
-        return cohortStartDate;
-    }
-
-    public void setCohortStartDate(LocalDate cohortStartDate) {
-        this.cohortStartDate = cohortStartDate;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 }
