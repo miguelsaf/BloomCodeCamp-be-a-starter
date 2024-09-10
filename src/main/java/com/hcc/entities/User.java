7d5b358d3e7a2c359b2f960c34371243c58a5e16
miguelsaf <miguelarcanjo03@gmail.com>
2024-09-10 11:55:24 +0100

    Creation of User entity stage 1

--- src/main/java/com/hcc/entities/User.java
+++ src/main/java/com/hcc/entities/User.java
@@ -1,4 +1,30 @@
 package com.hcc.entities;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "cohortStartDate")
+    private LocalDate cohortStartDate;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    public User() {
+    }
+
+    public User(LocalDate cohortStartDate, String username, String password) {
+        this.cohortStartDate = cohortStartDate;
+        this.username = username;
+        this.password = password;
+    }
 }
