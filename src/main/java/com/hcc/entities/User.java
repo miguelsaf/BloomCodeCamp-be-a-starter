9b50e77ac4a386647bfc0c37ba8b91394bcad786
miguelsaf <miguelarcanjo03@gmail.com>
2024-09-10 12:23:52 +0100

    addition of gethers and setters for username cohortDate and password

--- src/main/java/com/hcc/entities/User.java
+++ src/main/java/com/hcc/entities/User.java
@@ -27,4 +27,28 @@ public class User {
         this.username = username;
         this.password = password;
     }
+
+    public LocalDate getCohortStartDate() {
+        return cohortStartDate;
+    }
+
+    public void setCohortStartDate(LocalDate cohortStartDate) {
+        this.cohortStartDate = cohortStartDate;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
